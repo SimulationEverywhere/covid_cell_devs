6073e74c31d61a3a8f024ef761f324f267ccc6fd
Cristina Ruiz <cruizm.89@gmail.com>
2020-04-07 14:14:11 -0400

    initial commit

---
+++ Covid_testingkk.bat
@@ -0,0 +1,4 @@
+cd /D "C:\eclipse\WS_covid\Covid"
+"C:/eclipse/plugins/cdBuilder.simulator_1.0.0.201010092241/internal/simuOrig.exe"  -m"covid_death.ma" -l"covid_death_100LOG_3.log" -t"00:10:00:000" 
+rem 
+pause
